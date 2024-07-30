acbdec3809a4077ac8c3231d7a6b94421b89a7e9
Seokwoodang <swoo1429@naver.com>
2024-07-30 09:54:49 +0900

    create : naver api 사용할 페이지 생성

---
+++ app/map/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+/** naver map api를 사용해보자 */
+export default function MapPage() {
+  return (
+    <div>
+      MapPage is here!!
+      <div>
+        <Link href={"/"}>mainPage</Link>
+      </div>
+    </div>
+  );
+}
